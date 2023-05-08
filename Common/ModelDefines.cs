8cf150927fe9fccc6bf1a6d454b969653650ff62
dabet <dabetick@gmail.com>
2023-05-08 13:45:00 +0200

    Add project files.

---
+++ Common/ModelDefines.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FTN.Common
+{
+	
+	public enum DMSType : short
+	{		
+		MASK_TYPE							= unchecked((short)0xFFFF),
+
+		CURVE								= 0x0001,
+		CURVEDATA							= 0x0002,
+		REGULARTIMEPOINT					= 0x0003,
+		IRREGULARTIMEPOINT					= 0x0004,
+		OUTAGESCHEDULE						= 0x0005,
+        REGULARINTERVALSCHEDULE				= 0x0006,
+        PROTECTEDSWITCH						= 0x0007,
+       
+    }
+
+    [Flags]
+    public enum ModelCode : long
+    {
+        IDOBJ                               = 0x1000000000000000,
+        IDOBJ_GID                           = 0x1000000000000104,
+        IDOBJ_ALIASNAME                     = 0x1000000000000207,
+        IDOBJ_MRID                          = 0x1000000000000307,
+        IDOBJ_NAME                          = 0x1000000000000407,
+
+        PSR                                 = 0x1100000000000000,
+        PSR_OUTAGESCHEDULE                  = 0x1100000000000109,
+
+        REGULARTIMEPOINT                    = 0x1200000000030000,
+        REGULARTIMEPOINT_SEQUENCENUMBER     = 0x1200000000030104,
+        REGULARTIMEPOINT_VALUE1             = 0x1200000000030205,
+        REGULARTIMEPOINT_VALUE2             = 0x1200000000030305,
+        REGULARTIMEPOINT_INTERVALSCHEDULE   = 0x1200000000030409,
+
+        CURVEDATA                           = 0x1300000000020000,
+        CURVEDATA_XVALUE                    = 0x1300000000020105,
+        CURVEDATA_Y1VALUE                   = 0x1300000000020205,
+        CURVEDATA_Y2VALUE                   = 0x1300000000020305,
+        CURVEDATA_Y3VALUE                   = 0x1300000000020405,
+        CURVEDATA_CURVE                     = 0x1300000000020505,
+
+        CURVE                               = 0x1400000000010000,
+        // CURVE_CURVESTYLE = 0x1300000000010000,
+        // DA LI OVO STAVLJATI
+
+        //CURVE_XMULTIPLIER                   = 0x1400000000010105,
+        //CURVE_XUNIT                         = 0x1400000000010205,
+        //CURVE_Y1MULTIPLIER                  = 0x1400000000010305,
+        //CURVE_Y1UNIT                        = 0x1400000000010405,
+        //CURVE_Y2MULTIPLIER                  = 0x1400000000010505,
+        //CURVE_Y2UNIT                        = 0x1400000000010605,
+        //CURVE_Y3MULTIPLIER                  = 0x1400000000010705,
+        //CURVE_Y3UNIT                        = 0x1400000000010805,
+        //CURVE_CURVEDATAS                    = 0x1400000000010919,
+        // ZA OVO TREBA UNITSYMBOL I UNITMULTIPLIER ENUM UMESTO 05 NA KRAJU
+
+
+        BASICINTERVALSCHEDULE = 0x1500000000000000,
+        BASICINTERVALSCHEDULE_STARTTIME          = 0x1500000000000108,
+        //BASICINTERVALSCHEDULE_VALUE1MULTIPLIER   = 0x1500000000000219,
+        //BASICINTERVALSCHEDULE_VALUE1UNIT         = 0x1500000000000319,
+        //BASICINTERVALSCHEDULE_VALUE2MULTIPLIER   = 0x1500000000000419,
+        //BASICINTERVALSCHEDULE_VALUE2UNIT         = 0x1500000000000519,
+
+        // ZA OVO TREBA UNITSYMBOL I UNITMULTIPLIER ENUM UMESTO 05 NA KRAJU
+
+
+
+        IRREGULARTIMEPOINT                      = 0x1600000000040000,
+        //IRREGULARTIMEPOINT_TIME                 = 0x1600000000040100,
+
+        //OVDE TREBA SECONDS ZA ENUM?
+        IRREGULARTIMEPOINT_VALUE1               = 0x1600000000040205,
+        IRREGULARTIMEPOINT_VALUE2               = 0x1600000000040305,
+        IRREGULARTIMEPOINT_INTERVALSCHEDULE     = 0x1600000000040409,
+
+
+        EQUIPMENT                               = 0x1100000000000000,
+        EQUIPMENT_AGGREGATE                     = 0x1100000000000101,
+        EQUIPMENT_NORMALLYSERVICE               = 0x1100000000000201,
+
+
+        CONDUCTINGEQUIPMENT                     = 0x1110000000000000,
+
+        SWITCH                                  = 0x1111000000000000,
+        SWITCH_NORMALOPEN                       = 0x1111000000000101,
+        //SWITCH_RATEDCURRENT= 0x1111000000000000,    
+        SWITCH_RETAINED                         = 0x1111000000000201,
+        SWITCH_SWITCHONCOUNT                    = 0x1111000000000304,
+        SWITCH_SWITCHONDATE                     = 0x1111000000000408,
+
+
+        PROTECTEDSWITCH                         = 0x1111100000070000,
+        
+        REGULARINTERVALSCHEDULE                 = 0x1510000000060000,
+        REGULARINTERVALSCHEDULE_ENDTIME         = 0x1510000000060108,
+        //REGULARINTERVALSCHEDULE_TIMESTEP        = 0x1510000000060200,
+        REGULARINTERVALSCHEDULE_TIMEPOINTS      = 0x1510000000060319,
+
+        IRREGULARINTERVALSCHEDULE               = 0x1520000000000000,
+        IRREGULARINTERVALSCHEDULE_TIMEPOINTS    = 0x1520000000000119,
+
+        OUTAGESCHEDULE                          = 0x1521000000050000,
+        OUTAGESCHEDULE_POWERSYSTEMRESOURCE      = 0x1521000000050109,
+
+    }
+
+    [Flags]
+	public enum ModelCodeMask : long
+	{
+		MASK_TYPE			 = 0x00000000ffff0000,
+		MASK_ATTRIBUTE_INDEX = 0x000000000000ff00,
+		MASK_ATTRIBUTE_TYPE	 = 0x00000000000000ff,
+
+		MASK_INHERITANCE_ONLY = unchecked((long)0xffffffff00000000),
+		MASK_FIRSTNBL		  = unchecked((long)0xf000000000000000),
+		MASK_DELFROMNBL8	  = unchecked((long)0xfffffff000000000),		
+	}																		
+}
+
+
